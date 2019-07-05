36417da6fd068bca04269f992243b77eb4c5db91
CuriousPi <47406933+CuriousPi@users.noreply.github.com>
2019-07-05 16:24:09 +1000

    sync slider

--- script.js
+++ script.js
@@ -9,9 +9,55 @@
       }
     });
 
+    handle.on("sliderChange", function (){
+      MyApp.emit("sliderChange", this, $(this).slider("value"));
+    });
+
     $( "button" ).click( function( event ) {
       TogetherJS(this); 
       return false;
-    } );
+    });
+
+TogetherJSConfig_on_ready = function () {
+  MyApp.on("sliderChange", fireTogetherJSVisibility);
+};
+TogetherJSConfig_on_close = function () {
+  MyApp.off("sliderChange", fireTogetherJSVisibility);
+};
+
 
   } );
+
+var sliderChangeFromRemote = false;
+
+function fireTogetherJSVisibility(element, value) {
+  if(sliderChangeFromRemote){
+  	return;
+  }
+  var elementFinder = TogetherJS.require("elementFinder");
+  var location = elementFinder.elementLocation(element);
+  TogetherJS.send({type: "sliderChange", value: value, element: location});
+}
+
+
+TogetherJS.hub.on("sliderChange", function (msg) {
+  if(!msg.sameUrl){
+  	return;
+  }
+  var elementFinder = TogetherJS.require("elementFinder");
+  // If the element can't be found this will throw an exception:
+  var element = elementFinder.findElement(msg.element);
+  sliderChangeFromRemote = true;
+  try{
+   MyApp.updateSlider(element, msg.value);
+   }
+  finally{
+  	sliderChangeFromRemote = false;
+  }
+});
+
+MyApp.updateSlider = function(el, value){
+
+	    $('#custom-handle').slider('value', value);
+
+}
